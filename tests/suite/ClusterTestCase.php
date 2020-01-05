1d2be5179c2ef18c63119e816500a018a5e81adf
Robert Kummer <rok@ipunkt.biz>
2020-01-05 12:10:52 +0100

    whitespace

--- tests/suite/ClusterTestCase.php
+++ tests/suite/ClusterTestCase.php
@@ -2,18 +2,18 @@
 
 class ClusterTestCase extends BaumTestCase
 {
-    public static function setUpBeforeClass(): void
-    {
-        with(new ClusterMigrator())->up();
-    }
+	public static function setUpBeforeClass(): void
+	{
+		(new ClusterMigrator())->up();
+	}
 
-    public function setUp(): void
-    {
-        with(new ClusterSeeder())->run();
-    }
+	public function setUp(): void
+	{
+		(new ClusterSeeder())->run();
+	}
 
-    protected function clusters($name, $className = 'Cluster')
-    {
-        return forward_static_call_array([$className, 'where'], ['name', '=', $name])->first();
-    }
+	protected function clusters($name, $className = 'Cluster')
+	{
+		return forward_static_call_array([$className, 'where'], ['name', '=', $name])->first();
+	}
 }
